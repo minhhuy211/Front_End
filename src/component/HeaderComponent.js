e6b7e6f761e4a76924f43d1b0c5e5157cfc3eaeb
nguyenvanminhhuy <huycattuong0202@gmail.com>
2024-06-30 22:10:42 +0700

    setting

---
+++ src/component/HeaderComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../style/Header.scss";
+ 
+class HeaderComponent extends React.Component{
+    render() {
+        return <div className= "header">asss</div>
+
+    }
+}
+
+
+export default HeaderComponent;
