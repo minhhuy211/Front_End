c17966736d17a1121b48ee61f2db0169ab680671
nguyenvanminhhuy <huycattuong0202@gmail.com>
2024-07-16 20:43:59 +0700

    update home

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import Header from './component/Header';
+import Header from "./component/Header";
 import Footer from "./component/Footer";
-import "../src/asset/style.css"
- import "../src/vendors/bootstrap/bootstrap.min.css"
+import "../src/asset/style.css";
+import "../src/vendors/bootstrap/bootstrap.min.css";
 
 function App() {
   return (
-      <div>
-    <Header/>
-      <Footer/>
-      </div>
+    <div>
+      <Header />
+      <Footer />
+    </div>
   );
 }
 
