29dd89148e44996946ed5a609e164acd2b8ab735
Ngọc Thảo <21130542@st.hcmuaf.edu.vn>
2024-07-17 17:13:55 +0700

    thêm chuyển trang và giao diện trang products

---
+++ src/component/Guide.tsx
@@ -0,0 +1,12 @@
+import React  from "react";
+class Guide extends React.Component {
+    render() {
+        return(
+            <>
+                <h4>Đây là trang Guide</h4>
+            </>
+        );
+    }
+}
+
+export default Guide;
--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,71 +1,77 @@
-import React, { useState } from "react";
-// import "../asset/style.css";
+import React, {useState} from "react";
 import logo from "../assets/logo.png";
 import "../styles/Header.scss";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {NavLink} from "react-router-dom"
+
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {
-  faSearch,
-  faShoppingCart,
-  faUser,
+    faSearch,
+    faShoppingCart,
+    faUser,
 } from "@fortawesome/free-solid-svg-icons";
-
-const Header = () => {
-  const [header, setHeader] = useState("home");
-
-  return (
-    <div className="header-area">
-      <nav className="navbar navbar-expand-lg navbar-light">
-        <div className="container">
-          <a href="#">
-            <img src={logo} alt="" className="logo" />
-          </a>
-          <ul className="nav navbar-nav menu_nav ml-auto mr-auto">
-            <li
-              onClick={() => setHeader("home")}
-              className={header === "home" ? "active" : ""}
-            >
-              Trang Chủ
-            </li>
-            <li
-              onClick={() => setHeader("products")}
-              className={header === "products" ? "active" : ""}
-            >
-              Code Tham Khảo
-            </li>
-            <li
-              onClick={() => setHeader("guide")}
-              className={header === "guide" ? "active" : ""}
-            >
-              Hướng Dẫn
-            </li>
-            <li
-              onClick={() => setHeader("contact")}
-              className={header === "contact" ? "active" : ""}
-            >
-              Liên Hệ
-            </li>
-          </ul>
-          <ul className="nav-shop">
-            <li className="nav-item">
-              <FontAwesomeIcon icon={faSearch}></FontAwesomeIcon>
-            </li>
-            <li className="nav-item">
-              <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
-            </li>
-            <li className="nav-item">
-              <a className="button button-header" href="#">
-                <FontAwesomeIcon
-                  icon={faUser}
-                  className="header-icon-button"
-                ></FontAwesomeIcon>
-                Sing In
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </div>
-  );
-};
+class Header extends React.Component {
+    render() {
+        return (
+            <div className="header-area">
+                <nav className="navbar navbar-expand-lg navbar-light">
+                    <div className="container">
+                        <a href="#">
+                            <img src={logo} alt="" className="logo"/>
+                        </a>
+                        <ul className="nav navbar-nav menu_nav ml-auto mr-auto">
+                            <li >
+                            <NavLink
+                                to="/"
+                                className={({ isActive }) => (isActive ? "active" : "")}
+                            >
+                                Trang Chủ
+                            </NavLink>
+                            </li>
+                            <li>
+                            <NavLink
+                                to="/products"
+                                className={({ isActive }) => (isActive ? "active" : "")}
+                            >
+                                Code Tham Khảo
+                            </NavLink></li>
+                                <li>
+                            <NavLink
+                                to="/guide"
+                                className={({ isActive }) => (isActive ? "active" : "")}
+                            >
+                                Hướng Dẫn
+                            </NavLink></li>
+                            <li>
+                            <NavLink
+                                to="/contact"
+                                className={({ isActive }) => (isActive ? "active" : "")}
+                            >
+                                Liên Hệ
+                            </NavLink>
+                            </li>
+                        </ul>
+                        <ul className="nav-shop">
+                            <li className="nav-item">
+                                <FontAwesomeIcon icon={faSearch}></FontAwesomeIcon>
+                            </li>
+                            <li className="nav-item">
+                                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+                            </li>
+                            <li className="nav-item">
+                                <a className="button button-header" href="#">
+                                    <FontAwesomeIcon
+                                        icon={faUser}
+                                        className="header-icon-button"
+                                    ></FontAwesomeIcon>
+                                    Đăng nhập
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        );
+    };
+}
 
 export default Header;
